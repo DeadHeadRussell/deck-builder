584050604b7745b46a522bcf8daa9abf2686118c
Andrew Russell <deadhead.russell@gmail.com>
2015-11-02 04:05:26 -0500

    Added draft server.
    Is probably kinda buggy.
    Is really hacky.

--- compile.sh
+++ compile.sh
@@ -3,9 +3,11 @@
 if [ "$1" = "--watch" ]
 then
   echo 'watching...'
-  watchify -t babelify ./js/entry.js -o ./public/entry.js
+  watchify -t babelify ./js/entry.js -o ./public/entry.js &
+  babel ./js/models/* ./js/data/* ./server/* --watch -d ./build
 else
   echo 'compiling...'
-  browserify -t babelify ./js/entry.js > ./public/js/entry.js
+  browserify -t babelify ./js/entry.js > ./public/entry.js
+  babel ./js/models/* ./js/data/* ./server/* -d ./build
 fi
 
--- js/pages/draft.js
+++ js/pages/draft.js
@@ -1,63 +1,102 @@
 import React from 'react';
 
+import * as Cards from '../models/cards';
 import CardsList from '../components/cardsList';
 import Board from '../components/board';
 import Pack from '../models/pack';
+import Table from '../components/table';
+
+let socket = io('http://mtg.ajrussell.ca/draft');
 
 export default class Draft extends React.Component {
   constructor() {
     super();
-    this.updateNumDrafters = this.updateNumDrafters.bind(this);
-    this.newPack = this.newPack.bind(this);
-    this.updatePack = this.updatePack.bind(this);
+    this.createDraft = this.createDraft.bind(this);
+    this.startDraft = this.startDraft.bind(this);
     this.selectCard = this.selectCard.bind(this);
     this.toSideboard = this.toSideboard.bind(this);
     this.toMainboard = this.toMainboard.bind(this);
+
+    this.draftId = 0;
 
     this.roundNum = 1;
     this.packNum = -1;
 
     this.state = {
-      numDrafters: 2,
+      draftList: [],
+      history: [],
+      ended: false,
+      inDraft: false,
       selected: false,
+      started: false,
       pack: null,
       cards: [],
-      sideCards: []
+      sideCards: [],
+      table: []
     };
 
-    this.state.pack = this.createPack(new Pack());
+    socket.on('draftList', data => this.setState({draftList: data}));
+    socket.on('history', data => this.setState({history: data}));
+
+    socket.on('start', data => {
+      this.draftId = data.draftId;
+      this.setState({started: true});
+    });
+
+    socket.on('nextPack', data => {
+      let pack = Pack.FROM_NAMES(data.pack);
+      this.roundNum = data.roundNum;
+      this.setState({pack: this.createPack(pack), selected: false});
+    });
+
+    socket.on('end', data => this.setState({ended: true, started: false}));
+    socket.on('table', data => {
+      this.draftId = data.draftId;
+      this.setState({table: data.table});
+    });
+
+    socket.on('rc', data => {
+      this.draftId = data.draftId;
+      this.setState({
+        inDraft: true,
+        started: data.started,
+        ended: data.ended,
+        pack: data.pack ? this.createPack(Pack.FROM_NAMES(data.pack)) : null,
+        selected: data.cardPicked,
+        sideCards: Cards.fromNames(data.cards)
+      });
+    });
+  }
+
+  createDraft() {
+    let name = prompt('Draft name');
+    if (name) {
+      socket.emit('create', {name});
+      this.setState({inDraft: true});
+    }
+  }
+
+  joinDraft(draft) {
+    socket.emit('join', {draftId: draft.id});
+    this.setState({inDraft: true});
+  }
+
+  rejoinDraft(draft) {
+    socket.emit('rc', {draftId: draft.id});
+    this.setState({inDraft: true});
   }
 
   createPack(pack) {
+    let numDrafters = this.state.table.length;
+    this.packNum = (this.packNum + 1) % numDrafters;
 
-    this.packNum = (this.packNum + 1) % this.state.numDrafters;
-    let packNum = this.packNum + ((this.roundNum - 1) * this.state.numDrafters) + 1;
-
+    let packNum = this.packNum + ((this.roundNum - 1) * numDrafters) + 1;
     pack.cards.forEach(card => card.pack = packNum);
     return pack;
   }
 
-  updateNumDrafters(event) {
-    let num = parseInt(event.target.value, 10);
-    if (!num > 0) {
-      num = null;
-    }
-    this.setState({numDrafters: num});
-  }
-
-  newPack() {
-    this.roundNum++;
-    this.packNum = -1;
-    this.setState({pack: this.createPack(new Pack()), selected: false});
-  }
-
-  updatePack(cardNames) {
-    let pack = Pack.FROM_NAMES(cardNames);
-    if (pack.cards.length > 0) {
-      this.setState({pack: this.createPack(pack), selected: false});
-    } else {
-      this.setState({pack: null, selected: false});
-    }
+  startDraft() {
+    socket.emit('start', {draftId: this.draftId});
   }
 
   selectCard(card) {
@@ -71,6 +110,7 @@ export default class Draft extends React.Component {
       this.state.sideCards.push(card);
       this.state.selected = true;
       this.setState(this.state);
+      socket.emit('pickCard', {draftId: this.draftId, cardId: index});
     }
   }
 
@@ -93,6 +133,48 @@ export default class Draft extends React.Component {
   }
 
   render() {
+    if (!this.state.inDraft) {
+      return this.renderDraftList();
+    } else if (this.state.started) {
+      return this.renderDraft();
+    } else if (this.state.ended) {
+      return this.renderBoards();
+    } else {
+      return this.renderIdle();
+    }
+  }
+
+  renderDraftList() {
+    return (
+      <div className='draft'>
+        <h1 className='header'>Drafts</h1>
+        <div className='draft-controls'>
+          <h2 className='subheader'>Draft List</h2>
+          <button className='draft-control' onClick={this.createDraft}>Create Draft</button>
+          <ul className='draft-list'>{this.state.draftList.map(draft => (
+            <li key={draft.id} className='draft-list-item'>
+              <button className='draft-control' onClick={this.joinDraft.bind(this, draft)}>
+                {draft.name}
+              </button>
+            </li>
+          ))}</ul>
+        </div>
+
+        <div className='draft-controls'>
+          <h2 className='subheader'>History</h2>
+          <ul className='draft-list'>{this.state.history.map(draft => (
+            <li key={draft.id} className='draft-list-item'>
+              <button className='draft-control' onClick={this.rejoinDraft.bind(this, draft)}>
+                {draft.name}
+              </button>
+            </li>
+          ))}</ul>
+        </div>
+      </div>
+    );
+  }
+
+  renderDraft() {
     let messageText = this.state.selected ?
       'You have selected a card from this pack' :
       'Please select a card from this pack';
@@ -104,13 +186,12 @@ export default class Draft extends React.Component {
         <h1 className='header'>Draft</h1>
 
         <div className='draft-controls'>
+          <h2 className='subheader'>Players</h2>
+          <Table players={this.state.table} />
+        </div>
+
+        <div className='draft-controls'>
           <h2 className='subheader'>Card Selection</h2>
-          <label className='draft-control'>
-            <p>Number of drafters:</p>
-            <input className='draft-num-drafters' value={this.state.numDrafters} onChange={this.updateNumDrafters} />
-          </label>
-          <button className='draft-control' onClick={this.newPack}>Generate Pack</button>
-          <div className='draft-control'><CardsList cards={packCards} counts={false} onChange={this.updatePack} /></div>
           <p className='draft-control'>{messageText}</p>
           <Board name='Draft Pack' cards={packCards} onCardClick={this.selectCard} defaultSort='pack' />
         </div>
@@ -120,6 +201,44 @@ export default class Draft extends React.Component {
           <div className='draft-control'><CardsList cards={this.state.cards} sideboard={this.state.sideCards} /></div>
           <Board name='Main Board' cards={this.state.cards} onCardClick={this.toSideboard} defaultRows={true} />
           <Board name='Side Board' cards={this.state.sideCards} onCardClick={this.toMainboard} defaultSort='pack' />
+        </div>
+      </div>
+    );
+  }
+
+  renderBoards() {
+    return (
+      <div className='draft'>
+        <h1 className='header'>Draft</h1>
+
+        <p className='draft-control'>Draft completed</p>
+
+        <div className='draft-controls'>
+          <h2 className='subheader'>Your Cards</h2>
+          <div className='draft-control'><CardsList cards={this.state.cards} sideboard={this.state.sideCards} /></div>
+          <Board name='Main Board' cards={this.state.cards} onCardClick={this.toSideboard} defaultRows={true} />
+          <Board name='Side Board' cards={this.state.sideCards} onCardClick={this.toMainboard} defaultSort='pack' />
+        </div>
+
+        <div className='draft-controls'>
+          <h2 className='subheader'>Players</h2>
+          <Table players={this.state.table} />
+        </div>
+      </div>
+    );
+  }
+
+  renderIdle() {
+    return (
+      <div className='draft'>
+        <h1 className='header'>Draft</h1>
+        <div className='draft-controls'>
+          <h2 className='subheader'>Players</h2>
+          <Table players={this.state.table} />
+        </div>
+
+        <div className='draft-controls'>
+          <button className='draft-control' onClick={this.startDraft}>Start Draft</button>
         </div>
       </div>
     );
